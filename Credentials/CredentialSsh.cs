a576f4ef246d42d939d0037b62d1980a2b6bcf80
Eddie Krill <scherenhaenden@gmail.com>
2020-12-17 13:00:46 +0100

    Create ssh config model for connections

---
+++ Credentials/CredentialSsh.cs
@@ -0,0 +1,10 @@
+namespace TunnelConnector.Credentials
+{
+    public class CredentialSsh
+    {
+        public string Host { get; init; }
+        public int Port { get; init; }
+        public string User { get; init; }
+        public string Password { get; init; }
+    }
+}
