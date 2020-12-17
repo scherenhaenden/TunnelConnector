8983e7863ce8e677d7813f6cd827e011a20e8bf5
Eddie Krill <scherenhaenden@gmail.com>
2020-12-17 13:26:32 +0100

    Changes:
    -Make obsolete old ctor
    -use config models to initiat ssh and loadblancer

--- LoadBalancer/LoadBalancer.cs
+++ LoadBalancer/LoadBalancer.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections.Generic;
+using TunnelConnector.Credentials;
+using TunnelConnector.Protocls;
 using TunnelConnector.Protocls.SSH;
 using TunnelConnector.TunnelingEngine;
 
@@ -13,12 +16,27 @@ namespace TunnelConnector.LoadBalancer
         private readonly string _user;
         private readonly string _password;
         private readonly int _protocolPort;
+        private readonly AvailableProtocols _currentProtocol;
         private readonly string _protocol;
         private List<ITunnelCreator> _tunnelCreators;
         private bool _initiated = false;
 
         private int LastUseId = 0;
-
+        
+        
+        public LoadBalancer(LoadBalancerConfiguration loadBalancerConfiguration)
+        {
+            _localhost = loadBalancerConfiguration.Localhost;
+            _localPorts = loadBalancerConfiguration.LocalPorts;
+            _foreignHost = loadBalancerConfiguration.ForeignHost;
+            _foreignPort = loadBalancerConfiguration.ForeignPort;
+            _user = loadBalancerConfiguration.User;
+            _password = loadBalancerConfiguration.Password;
+            _protocolPort = loadBalancerConfiguration.ProtocolPort;
+            _currentProtocol = loadBalancerConfiguration.CurrentProtocol;
+        }
+        
+        [Obsolete]
         public LoadBalancer(string localhost, int [] localPorts, string foreignHost, int foreignPort, string user, string password, int protocolPort, string protocol = "SSH")
         {
             _localhost = localhost;
@@ -29,6 +47,10 @@ namespace TunnelConnector.LoadBalancer
             _password = password;
             _protocolPort = protocolPort;
             _protocol = protocol;
+            if (_protocol == "SSH")
+            {
+                _currentProtocol = AvailableProtocols.Ssh;
+            }
         }
 
 
@@ -39,9 +61,12 @@ namespace TunnelConnector.LoadBalancer
             {
                 //IProtocolClient sshConnector;
                 
-                if (_protocol == "SSH")
+                if (_currentProtocol == AvailableProtocols.Ssh)
                 {
-                    var sshConnector = new SshConnector(_foreignHost, _protocolPort, _user, _password, null);
+                    var sshConnector = new SshConnector(
+                        new CredentialSsh()
+                            {Host = _foreignHost, Port = _protocolPort, User = _user, Password = _password},  
+                        null);
                     ITunnelCreator tunnelCreator = new TunnelCreator(sshConnector
                         , _localhost,localport , _foreignHost, _foreignPort );
                     _tunnelCreators.Add(tunnelCreator);
